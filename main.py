987f24ca644358eb090bf2b46b0eaa6c806c0e58
Nurzhan <mrb.nurzhan@gmail.com>
2022-11-29 23:56:09 +0600

    added post request

--- main.py
+++ main.py
@@ -1,29 +1,53 @@
-
-
-import pyscreenshot as ImageGrab
-
+import requests
+from PIL import ImageGrab
 import schedule
 import time
-
+import glob
+import os
 from datetime import datetime
 
+
+# def take_screenshot():
+#     print("Taking screenshot...")
+
+#     image_date = f"screenshot-{str(datetime.now())}"
+#     screenshot = ImageGrab.grab()
+#     image_list = list(image_date)
+#     image_list[25] = '.'
+#     image_list[28] = '.'
+#     image_list[30] = ','
+#     image_name = ''.join(image_list)
+#     filepath = f"./screenshots/{image_name}.png"
+#     #
+
+#     screenshot.save(f"{image_name}")
+
+#     # post = requests.post('http://127.0.0.1:8000/api/post-frames/', data={'frames': frame})
+
+#     print("Screenshot taken...")
+#     return filepath
 
 def take_screenshot():
     print("Taking screenshot...")
 
     image_date = f"screenshot-{str(datetime.now())}"
-    screenshot = ImageGrab.grab()
+    screenshot = ImageGrab.grab(bbox=None)
     image_list=list(image_date)
     image_list[24]='.'
     image_list[27]='.'
     image_name= ''.join(image_list)
-    filepath = f"./screenshots/{image_name}.png"
-
+    filepath = f"C:\\Users\\mrbnu\\Desktop\\Projects\\DreamLine\\screenshot_py_app\\screenshots\\{image_name}.png"
     screenshot.save(filepath)
-
-    print("Screenshot taken...")
+    send_img = open(f"C:\\Users\\mrbnu\\Desktop\\Projects\\DreamLine\\screenshot_py_app\\screenshots\\{image_name}.png", 'rb')
+    post = requests.post('http://127.0.0.1:8000/api/post-frames/', files={'frame': send_img})
+    
+    
+    print("Screenshot taken...",post.text)
 
     return filepath
+
+
+
 
 
 def main():
@@ -33,6 +57,6 @@ def main():
         schedule.run_pending()
         time.sleep(1)
 
-        
+
 if __name__ == '__main__':
     main()
